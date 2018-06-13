1f27ce7b8bdc7c2e42cc6e538bb6f4b63b511b88
zweiklwenq <zwk981990563@126.com>
2018-06-13 09:57:09 +0800

    Create about.py
    Description:
    1、Add the about.py

---
+++ UI/about.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'about.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_About(object):
+    def setupUi(self, About):
+        About.setObjectName("About")
+        About.resize(400, 147)
+        self.textBrowser = QtWidgets.QTextBrowser(About)
+        self.textBrowser.setGeometry(QtCore.QRect(110, 10, 151, 91))
+        self.textBrowser.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
+        self.textBrowser.setOpenLinks(False)
+        self.textBrowser.setObjectName("textBrowser")
+
+        self.retranslateUi(About)
+        QtCore.QMetaObject.connectSlotsByName(About)
+
+    def retranslateUi(self, About):
+        _translate = QtCore.QCoreApplication.translate
+        About.setWindowTitle(_translate("About", "About"))
+        self.textBrowser.setHtml(_translate("About", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-weight:600;\">Author:StoneY</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-weight:600;\">Version:V1.0</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-weight:600;\">Date:2018-05-30</span></p></body></html>"))
+
